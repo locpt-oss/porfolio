04fdea946e75223beeaaf8def97404e11f86616b
vict_ph <phan.thanhloc1705@gmail.com>
2022-08-10 10:57:16 +0700

    Tailwind CSS + Add package-lock.json to gitignore + Setup tailwind CSS + Add prefix "tw" for tailwind CSS + Add aspect ratio to tailwind CSS module + Adjust globals.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-    return <h1>Hello</h1>
+    return (
+        <>
+            <h1 className="tw-text-red-500">Body here</h1>
+        </>
+    )
 }
 
 export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    prefix: 'tw-',
+    content: [
+        './layouts/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            aspectRatio: {
+                '3/4': '3 / 4',
+                '4/3': '4 / 3',
+                '16/10': '16 / 10',
+                '21/9': '21 / 9',
+            },
+        },
+    },
+    plugins: [],
+}
